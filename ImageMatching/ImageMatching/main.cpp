765381675b764c1f3718eaf0d56ba44a5bd16396
罗忆 <luoyi@luoyidemacbook-air.local>
2016-03-28 23:10:56 +0800

    init

---
+++ ImageMatching/ImageMatching/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ImageMatching
+//
+//  Created by 罗忆 on 16/3/28.
+//  Copyright © 2016年 tohsssik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
